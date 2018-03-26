d8d21e10b9d5a8c1502246ebf38a046fb55ed267
BrandonGreet <greet.brandon@yahoo.com>
2018-03-25 22:16:19 -0400

    First draft
    First completed program.

--- InsertionSort/Program.cs
+++ InsertionSort/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//This program sorts a given array using insertion sort.
+using System;
+using System.IO;
 
 namespace InsertionSort
 {
@@ -6,7 +8,38 @@ namespace InsertionSort
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Initializing reader, writer, and bigArray
+            StreamReader reader = new StreamReader("/Users/BrandonGreet/Desktop/bigArray.txt");
+            int[] bigArray = new int[10000];
+            string entry;
+            int line = 0;
+            while((entry = reader.ReadLine()) != null)
+            {
+                bigArray[line] = int.Parse(entry);
+                line++;
+            }
+            reader.Close();
+
+
+            //Sorts by moves entries left until nothing before them is greater.
+            int curIndex;
+            int tempVal;
+            for (int i = 1; i < 10000; i++)
+            {
+                curIndex = i;
+                while (curIndex != 0 && bigArray[curIndex] < bigArray[curIndex - 1])
+                {
+                    tempVal = bigArray[curIndex];
+                    bigArray[curIndex] = bigArray[curIndex - 1];
+                    bigArray[curIndex - 1] = tempVal;
+                    curIndex--;
+                }
+            }
+
+            foreach (int item in bigArray)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
